5c96609a7a7dc33c1a164429e6b1ccb4b77cec3a
Beomman-Han <qjaaks6378@gmail.com>
2022-01-16 17:07:37 +0900

    week3

--- Beomman/week2/chapter3_test.py
+++ Beomman/week2/chapter3_test.py
@@ -1,7 +1,9 @@
 import copy
 
 r1 = ['John', ('man', 'USA'), [175,23]]
-r2 = copy.deepcopy(r1)
+r2 = copy.deepcopy(r1)  ## deep copy
+r3 = list(r1)  ## shallow copy
+r4 = r1[:]  ## shallow copy
 
 r1 is r2
 r1 == r2
